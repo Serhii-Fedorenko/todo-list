26390594d8504e08428730e3e081274dd0c47d41
SerhiiFedorenko <serhii.fedorenko90@gmail.com>
2024-04-12 19:24:00 +0300

    beautify TaskForm

--- src/Components/TaskForm/TaskForm.jsx
+++ src/Components/TaskForm/TaskForm.jsx
@@ -1,5 +1,8 @@
 import { useDispatch } from "react-redux";
 import { addTask } from "../../redux/operations";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
 
 const TaskForm = () => {
   const dispatch = useDispatch();
@@ -7,13 +10,29 @@ const TaskForm = () => {
   const handleFormSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
-    dispatch(addTask(form.elements.text.value))
-    form.reset()
+    dispatch(addTask(form.elements.text.value));
+    form.reset();
   };
+
   return (
     <form onSubmit={handleFormSubmit}>
-      <input type="text" name="text" placeholder="Enter task text..." />
-      <button type="submit">Add task</button>
+      <Grid container spacing={1} alignItems="center" marginTop='20px'>
+        <Grid item xs={2}>
+          <TextField
+            id="outlined-basic"
+            label="Enter task text..."
+            variant="outlined"
+            size="small"
+            type="text"
+            name="text"
+          />
+        </Grid>
+        <Grid item xs={8}>
+          <Button type="submit" variant="outlined" >
+            Add task
+          </Button>
+        </Grid>
+      </Grid>
     </form>
   );
 };
